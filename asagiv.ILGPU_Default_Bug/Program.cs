a8955f9985345d480e5fdfa9fbfcc0681051612c
Ari <DonSagiv@users.noreply.github.com>
2022-06-20 18:32:01 -0400

    Remove Other Projects

---
+++ asagiv.ILGPU_Default_Bug/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asagiv.ILGPU_Default_Bug
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
